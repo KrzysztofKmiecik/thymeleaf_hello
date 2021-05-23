e88c370aa1be686e16e03dd020978b93e9dbd2c0
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-05-23 14:34:52 +0200

    dodanieElementowZWidoku

--- src/main/java/pl/kmiecik/thymeleaf_hello/CarController.java
+++ src/main/java/pl/kmiecik/thymeleaf_hello/CarController.java
@@ -3,23 +3,39 @@ package pl.kmiecik.thymeleaf_hello;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-@GetMapping("/cars")
-    public String getCar(Model model){
-    Car car1=new Car("BMW","i7");
-    Car car2=new Car("Fiat","ducato");
-    Car car3=new Car("Polonez","Caro");
-    Car car4=new Car("Skoda","Octavia");
-    List<Car> carsList = Arrays.asList(car1,car2,car3,car4);
+    List<Car> carsList;
 
-     model.addAttribute("myCars",carsList);
-    return "myCar";
-}
+    public CarController() {
+        Car car1 = new Car("BMW", "i7");
+        Car car2 = new Car("Fiat", "ducato");
+        Car car3 = new Car("Polonez", "Caro");
+        Car car4 = new Car("Skoda", "Octavia");
+        carsList = new ArrayList<>();
+        carsList.add(car1);
+        carsList.add(car2);
+        carsList.add(car3);
+        carsList.add(car4);
+    }
 
+    @GetMapping("/car")
+    public String getCar(Model model) {
+        model.addAttribute("myCars", carsList);
+        model.addAttribute("newCar", new Car()); // do wykorzystania w post
+        return "myCar";
+    }
+
+    @PostMapping("/add-car")
+    public String addCar(@ModelAttribute Car car) {
+        carsList.add(car);
+        return "redirect:/car";
+    }
 }
